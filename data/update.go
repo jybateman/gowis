b4a81301af860777b5e1c854287ccb3aec74e870
jybateman <jeanyves.bateman@gmail.com>
2017-03-27 11:07:31 +0200

    DNS whois functionnal

--- data/update.go
+++ data/update.go
@@ -1,19 +1,49 @@
  package main
 
 import (
+	"os"
 	"fmt"
+	"time"
+	"bytes"
+	"regexp"
+	"strings"
 	"net/http"
+	"io/ioutil"
 
 	"golang.org/x/net/html"
 )
 
 const (
+	URL = "https://www.iana.org"
 	DNS = "https://www.iana.org/domains/root/db"
 	IPV4 = "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv"
 	IPV6 = "https://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.csv"
 	AS = "https://www.iana.org/assignments/as-numbers/as-numbers-1.csv"
 	AS32 = "https://www.iana.org/assignments/as-numbers/as-numbers-2.csv"
 )
+
+func getWhois(lnk string) string {
+	re := regexp.MustCompile(`\s+\S*$`)
+	resp, err := http.Get(lnk)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	pg, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	off := bytes.Index(pg, []byte("WHOIS Server:"))
+	if off < 0 {
+		return ""
+	}
+	end := bytes.Index(pg[off:], []byte("\n"))
+	if end < 0 {
+		return ""
+	}
+	return strings.Trim(string(re.Find(pg[off:off+end])), " ")
+}
 
 func findTable(n *html.Node) *html.Node {
 	if n.Data == "table" {
@@ -32,30 +62,50 @@ func findTable(n *html.Node) *html.Node {
 	return nil
 }
 
-func getLinks(n *html.Node, lvl int) {
+func getLinks(n *html.Node, lst *[]string) {
 	if n.Data == "a" {
-		for _, a := range n.Attr {
-			fmt.Println(a)
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			fmt.Println(c)
-		}
-		return
+		// fmt.Println(n.Attr[0].Val)
+		// fmt.Println(n.FirstChild.Data)
+		*lst = append(*lst, n.Attr[0].Val, n.FirstChild.Data)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		getLinks(c, lvl+1)
+		getLinks(c, lst)
 	}
 }
 
 func getDomain() {
-	resp, _ := http.Get(DNS)
-	doc, _ := html.Parse(resp.Body)
-	
+	var lst []string
+
+	f, err := os.Create("domain_list")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	resp, err := http.Get(DNS)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	n := findTable(doc)
-	getLinks(n, 0)
-	resp.Body.Close()
+	if n == nil {
+		return
+	}
+	getLinks(n, &lst)
+	for i := 0; i < len(lst); i += 2 {
+		f.WriteString(lst[i+1]+" "+getWhois(URL+lst[i])+"\n")
+		time.Sleep(time.Second*5)
+	}
 }
 
 func main() {
 	getDomain()
+	// getWhois("https://www.iana.org/domains/root/db/com.html")
+	// getWhois("https://www.iana.org/domains/root/db/abb.html")
 }

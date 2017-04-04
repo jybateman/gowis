df9689cc85ad7f571b0a7fc34def7e5ab02eb1b4
jybateman <jeanyves.bateman@gmail.com>
2017-04-04 16:01:38 +0200

    changed port from string to int

--- main.go
+++ main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"flag"
-	"strings"
 )
 
 var host string
-var port string
+var port int
 var version bool
 func init() {
 	flag.StringVar(&host, "h", "",  "-h HOST	explicitly query HOST")
-	flag.StringVar(&port, "p", "",  "-p PORT	use port number PORT")
+	flag.IntVar(&port, "p", 43,  "-p PORT	use port number PORT")
 	flag.BoolVar(&version, "v", false,  "Display version")
 }
 
 func main() {
 	flag.Parse()
-	// fmt.Println(flag.Args())
-	// flag.Usage()
-	args := strings.Join(flag.Args(), " ")
-	switch GetType(args) {
-	case IPV4:
-		fmt.Println(getIPWhois(args, "data/ipv4_list"))
-		break
-	case AS:
-		fmt.Println(getASWhois(args, "data/as_list"))
-		break
-	case AS32:
-		fmt.Println(getASWhois(args, "data/as32_list"))
-		break
-	case DNS:
-		fmt.Println(getDNSWhois(args))
-		break
-	default:
-		fmt.Println("No whois server is known for this kind of object.")
-	}
+	getHost()
 }
